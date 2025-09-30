bd3a283b764ec0fc69475602397e3d8373dc40ef
blackdenxif <dn555178@gmail.com>
2025-09-30 19:37:10 +0700

    demo class 1

---
+++ class_tong.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class hang_hoa {
+    private:
+        int ma_hang;
+        string ten_hang;
+        string loai_hang;
+        string don_vi_tinh;
+        int so_luong;
+        double gia_ban;
+        string nha_san_xuat;
+        string ngay_san_xuat;
+        string han_su_dung;
+        string mo_ta;
+    public:
+        
+
+};
+
+class kho_hang{
+    private:
+        vector<hang_hoa> ds_hang_hoa;
+    public:
+};
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "class_tong.cpp"
+using namespace std;
+
+
+int main(){
+	
+	
+	return 0;
+}
