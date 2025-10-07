7f97fbb7fc0ffd146adc175db80d7958bd44a50e
ĐỖ ĐỨC MẠNH <manh241230785@lms.utc.edu.vn>
2025-10-07 21:21:27 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,29 @@
 #include "class_tong.h"
 #include "function_class_hh.cpp"
 #include "function_class_kh.cpp"
+#include "GiaoDich.cpp"
+#include "QuanLyBanHang.cpp"
 using namespace std;
 
+int main() {
+    kho_hang kh;
+    QuanLyBanHang qlbh;
+    kh.doc_du_lieu("kho_hang.dat");
+    kh.them_hang_hoa();     
+    kh.hien_thi_danh_sach();   
+    kh.xoa_hang_hoa();     
+    kh.hien_thi_danh_sach(); 
 
-int main(){
-	kho_hang kh;
-	kh.doc_du_lieu("kho_hang.dat");
-	kh.them_hang_hoa();
-	kh.hien_thi_danh_sach();
-	kh.xoa_hang_hoa();
-	kh.hien_thi_danh_sach();
-	return 0;
+    // Quản lý bán hàng (phần 5)
+    qlbh.docFile();               
+    qlbh.taoHoaDon(kh.ds_hang_hoa); 
+    qlbh.xuatTatCaHoaDon();      
+
+    // Báo cáo & phân tích (phần 6)
+    qlbh.baoCaoDoanhThu();       
+    qlbh.sanPhamBanChay();      
+    // Lưu lại kho sau khi bán
+    kh.luu_du_lieu("kho_hang.dat", 2); // ghi dè lên đó :Đ
+
+    return 0;
 }
